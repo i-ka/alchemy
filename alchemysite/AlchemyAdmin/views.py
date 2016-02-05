95686c4ae7f65066c2ab9e4a9bc2722c454a9ac7
ФО-240003 Калинич Игнат Владимирович <i-ka2010@mail.ru>
2016-02-05 19:59:48 +0500

    Added simple form and element addition

--- alchemysite/AlchemyAdmin/views.py
+++ alchemysite/AlchemyAdmin/views.py
@@ -14,4 +14,20 @@ def remove_element(request,el_id):
     Element.objects.get(pk=el_id).delete()
     return redirect('/alch-admin')
     
-        
+def create_element(request):
+    status = False
+    if request.method == 'POST':
+        new_el = Element(
+            name = request.POST['name'],
+            first_recipe_el = int(request.POST['first_recipe_el']),
+            second_recipe_el = int(request.POST['second_recipe_el']),
+            discription = request.POST['discription'],
+            )
+        if new_el.check_conflict():
+            new_el.save()
+            status = 'Элемент добавлен'
+        else:
+            status = 'Ошибка:элемент из рецепта не существует!'
+    return render(request,'AlchemyAdmin/add_element_form.html',{
+        'status':status,
+        })
