048cfdb94ec3ebe6bb7120f30baef3111f058300
ФО-240003 Калинич Игнат Владимирович <i-ka2010@mail.ru>
2016-02-05 19:59:48 +0500

    добавление элемента и простейшая форма

--- alchemysite/AlchemyAdmin/urls.py
+++ alchemysite/AlchemyAdmin/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     url(r'^$',views.index, name='index'),
     url(r'del/(?P<el_id>[0-9]+)',views.remove_element, name='del'),
+    url(r'create-element',views.create_element, name='create-element'),
 ]
--- alchemysite/AlchemyCommon/models.py
+++ alchemysite/AlchemyCommon/models.py
@@ -13,5 +13,14 @@ class Element(models.Model):
         if(self.first_recipe_el==0 and self.second_recipe_el==0):return True
         else:return False
 
+    def check_conflict(self):
+        if not self.is_essential_element():
+            try:
+                f_el = Element.objects.get(pk = self.first_recipe_el)
+                s_el = Element.objects.get(pk = self.second_recipe_el)
+            except self.DoesNotExist:
+                return False
+        return True
+
     def __str__(self):
         return self.name
