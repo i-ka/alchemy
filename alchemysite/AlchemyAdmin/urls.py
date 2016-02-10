21bfa28eaef015ad3541526ad685dadd431b5ee2
ФО-240003 Калинич Игнат Владимирович <i-ka2010@mail.ru>
2016-02-10 15:09:11 +0500

    Updated element check for element remove

--- alchemysite/AlchemyAdmin/urls.py
+++ alchemysite/AlchemyAdmin/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     url(r'^$',views.index, name='index'),
     url(r'del/(?P<el_id>[0-9]+)',views.remove_element, name='del'),
     url(r'create-element',views.create_element, name='create-element'),
+    url(r'update_element/(?P<el_id>[0-9]+)',views.update_element, name='update-element'),
 ]
