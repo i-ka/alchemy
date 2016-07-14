18ee6897e4606c2feaca9b137eabde2333bcb30b
Vadim Chetkov <vadim.chetkov@gmail.com>
2016-07-15 02:00:08 +0500

    [WIP] Tabs with categories and elements
    +refactoring

--- alchemysite/static/js/obj_load_drag.js
+++ alchemysite/static/js/obj_load_drag.js
@@ -1,26 +1,23 @@
-$(document).ready(function()
-{
-	$.getJSON("http://localhost:8000/alch-admin/get-elements-by-category/1",function(data)
-	{
-		$("[name=first_recipe_el]").removeAttr("value");
-		$("[name=second_recipe_el]").removeAttr("value");
-		$(".nav-tabs").append($("<li>").addClass('active').append($("<a>").attr('data-toggle', 'tab').attr('href', '#getCatName').text('getCatName')));
-		$(".tab-content").append($("<div>").attr('id', 'getCatName').addClass("tab-pane in active btn-group-vertical"));
-		for (var i = 0; i < data.elements.length; i++) {
-			$("#getCatName").append($("<a>").addClass("btn btn-default select").text(data.elements[i].name).attr("el_id",data.elements[i].id));
-		}
-		$(".select").draggable
-		({
-			revert: true,
-			revertDuration: 0
-		});
-		$(".select").mouseover(function()
-		{
-			$(this).attr("id","selected").css("cursor","move");
-		}).mouseleave(function()
-		{
-			$(this).removeAttr("id","selected");
-		}); 	
-	});
+$(document).ready(function() {
+  $.getJSON("http://localhost:8000/alch-admin/get_categories",function(data) {
+    $("[name=first_recipe_el], [name=second_recipe_el]").removeAttr("value");
 	
-});		
+      for (var i = 0; i < data.categories.length; i++) {
+        $(".nav-tabs").append($("<li>").append($("<a>").attr("data-toggle", "tab").attr("href", "#" + data.categories[i].id).text(data.categories[i].name)));
+        $(".tab-content").append($("<div>").attr("id", data.categories[i].id).addClass("tab-pane btn-group-vertical"));
+
+        $.getJSON("http://localhost:8000/alch-admin/get-elements-by-category/" + data.categories[i].id,function(data) {
+          for (var j = 0; j < data.elements.length; j++) {
+            $("#1").append($("<a>").addClass("btn btn-default select").text(data.elements[j].name).attr("el_id", data.elements[j].id));
+          }
+
+          $(".select").draggable ({ revert: true, revertDuration: 0 });
+          $(".select").mouseover(function() {
+            $(this).attr("id", "selected").css("cursor", "move");
+          }).mouseleave(function() {
+            $(this).removeAttr("id","selected");
+          });
+        });
+      }
+  });
+});
