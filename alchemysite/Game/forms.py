330c7810acab4db6916d7c76096997bea59896d5
ФО-240003 Калинич Игнат Владимирович <i-ka2010@mail.ru>
2016-07-17 12:57:31 +0600

    Game/forms: Added css classes and placeholders to registration form.

--- alchemysite/Game/forms.py
+++ alchemysite/Game/forms.py
@@ -7,15 +7,20 @@ from django.contrib.auth.models import User
 
 class RegisterForm(forms.Form):
 
-    username = forms.CharField(max_length=30)
-    email = forms.EmailField()
+    username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                            'placeholder':'Username'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                                            'placeholder':'Email'}))
 
-    pass1 = forms.CharField(widget=forms.PasswordInput,
-                            label="Пароль",
+    pass1 = forms.CharField(label="Пароль",
                             min_length=6,
-                            max_length=30)
+                            max_length=30,
+                            widget=forms.PasswordInput(attrs={'class':'form-control',
+                                                              'placeholder':'Password'}))
 
-    pass2 = forms.CharField(widget=forms.PasswordInput, label="Пароль ещё раз")
+    pass2 = forms.CharField(label="Пароль ещё раз",
+                            widget=forms.PasswordInput(attrs={'class':'form-control',
+                                                                'placeholder':'Repeat Password'}))
 
     def clean_pass2(self):
         if (self.cleaned_data["pass2"] != self.cleaned_data.get("pass1", "")):
