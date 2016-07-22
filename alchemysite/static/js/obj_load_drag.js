952194da07d1bca823c3f38b81b820b5544d0132
Vadim Chetkov <vadim.chetkov@gmail.com>
2016-07-22 17:09:08 +0500

    static/js/: finished elements loading

--- alchemysite/static/js/obj_load_drag.js
+++ alchemysite/static/js/obj_load_drag.js
@@ -1,23 +1,27 @@
+function elementLoad(categoryId) {
+  $.getJSON("http://localhost:8000/alch-admin/get-elements-by-category/" + categoryId,function(elementData) {
+    for (var j = 0; j < elementData.elements.length; j++) {
+      $("#" + categoryId).append($("<a>").addClass("btn btn-default select").text(elementData.elements[j].name).attr("el_id", elementData.elements[j].id));
+
+      $(".select").draggable ({ revert: true, revertDuration: 0 });
+      $(".select").mouseover(function() {
+        $(this).attr("id", "selected").css("cursor", "move");
+      }).mouseleave(function() {
+        $(this).removeAttr("id","selected");
+      });
+    }
+  });
+}
+
 $(document).ready(function() {
-  $.getJSON("http://localhost:8000/alch-admin/get_categories",function(data) {
-    $("[name=first_recipe_el], [name=second_recipe_el]").removeAttr("value");
-	
-      for (var i = 0; i < data.categories.length; i++) {
-        $(".nav-tabs").append($("<li>").append($("<a>").attr("data-toggle", "tab").attr("href", "#" + data.categories[i].id).text(data.categories[i].name)));
-        $(".tab-content").append($("<div>").attr("id", data.categories[i].id).addClass("tab-pane btn-group-vertical"));
+  $.getJSON("http://localhost:8000/alch-admin/get_categories",function(categoryData) {
+    $("[name=first_recipe_el], [name=second_recipe_el]").removeAttr("value"); 
 
-        $.getJSON("http://localhost:8000/alch-admin/get-elements-by-category/" + data.categories[i].id,function(data) {
-          for (var j = 0; j < data.elements.length; j++) {
-            $("#1").append($("<a>").addClass("btn btn-default select").text(data.elements[j].name).attr("el_id", data.elements[j].id));
-          }
+    for (var i = 0; i < categoryData.categories.length; i++) {
+      $(".nav-tabs").append($("<li>").append($("<a>").attr("data-toggle", "tab").attr("href", "#" + categoryData.categories[i].id).text(categoryData.categories[i].name)));
+      $(".tab-content").append($("<div>").attr("id", categoryData.categories[i].id).addClass("tab-pane btn-group-vertical"));
 
-          $(".select").draggable ({ revert: true, revertDuration: 0 });
-          $(".select").mouseover(function() {
-            $(this).attr("id", "selected").css("cursor", "move");
-          }).mouseleave(function() {
-            $(this).removeAttr("id","selected");
-          });
-        });
-      }
+      elementLoad(categoryData.categories[i].id);
+    }
   });
 });
