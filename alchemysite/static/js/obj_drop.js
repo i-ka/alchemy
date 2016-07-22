35d68f1430f5e5e65ec00b3b3cbe9c03ea19c1d6
Vadim Chetkov <vadim.chetkov@gmail.com>
2016-07-22 17:22:11 +0500

    static/js: refactor

--- alchemysite/static/js/obj_drop.js
+++ alchemysite/static/js/obj_drop.js
@@ -1,45 +1,35 @@
-$(document).ready(function()
-{
-	$("#drop1").droppable
-	({
-		drop:function()
-		{
-			$(this).text($("#selected").text());
-			$("[name=first_recipe_el]").val($("#selected").attr("el_id"));
-			$(this).removeClass("btn-default").addClass("btn-info");
-		}
-	});
-	
-	$("#drop2").droppable
-	({
-		drop:function()
-		{
-			$(this).text($("#selected").text());
-			$("[name=second_recipe_el]").val($("#selected").attr("el_id"));
-			$(this).removeClass("btn-default").addClass("btn-info");
-		}
-	});
+$(document).ready(function() {
+  $("#drop1").droppable({
+    drop: function() {
+      $(this).text($("#selected").text());
+      $("[name=first_recipe_el]").val($("#selected").attr("el_id"));
+      $(this).removeClass("btn-default").addClass("btn-info");
+    }
+  });
+  $("#drop2").droppable({
+    drop: function() {
+      $(this).text($("#selected").text());
+      $("[name=second_recipe_el]").val($("#selected").attr("el_id"));
+      $(this).removeClass("btn-default").addClass("btn-info");
+    }
+  });
 
-	$("#drop1").click(function()
-	{
-		$(this).text("Элемент 1");
-		$("[name=first_recipe_el]").removeAttr("value");
-		$(this).removeClass("btn-info").addClass("btn-default");
-	});	
-	$("#drop2").click(function()
-	{
-		$(this).text("Элемент 2");
-		$("[name=second_recipe_el]").removeAttr("value");
-		$(this).removeClass("btn-info").addClass("btn-default");
-	});
-	
-	$("[name=orig_check]").click(function()
-	{
-		$("[name=first_recipe_el]").val("0");
-		$("[name=second_recipe_el]").val("0");
-		$("#drop1").text("Элемент 1");
-		$("#drop2").text("Элемент 2");
-		$("#drop1, #drop2").removeClass("btn-info").addClass("btn-default");
-	});
-	
-});		
+  $("#drop1").click(function() {
+    $(this).text("Элемент 1");
+    $("[name=first_recipe_el]").removeAttr("value");
+    $(this).removeClass("btn-info").addClass("btn-default");
+  });
+  $("#drop2").click(function() {
+    $(this).text("Элемент 2");
+    $("[name=second_recipe_el]").removeAttr("value");
+    $(this).removeClass("btn-info").addClass("btn-default");
+  });
+
+  $("[name=orig_check]").click(function() {
+    $("[name=first_recipe_el]").val("0");
+    $("[name=second_recipe_el]").val("0");
+    $("#drop1").text("Элемент 1");
+    $("#drop2").text("Элемент 2");
+    $("#drop1, #drop2").removeClass("btn-info").addClass("btn-default");
+  });
+});
--- alchemysite/static/js/obj_load_drag.js
+++ alchemysite/static/js/obj_load_drag.js
@@ -15,7 +15,7 @@ function elementLoad(categoryId) {
 
 $(document).ready(function() {
   $.getJSON("http://localhost:8000/alch-admin/get_categories",function(categoryData) {
-    $("[name=first_recipe_el], [name=second_recipe_el]").removeAttr("value"); 
+    $("[name=first_recipe_el], [name=second_recipe_el]").removeAttr("value");
 
     for (var i = 0; i < categoryData.categories.length; i++) {
       $(".nav-tabs").append($("<li>").append($("<a>").attr("data-toggle", "tab").attr("href", "#" + categoryData.categories[i].id).text(categoryData.categories[i].name)));
