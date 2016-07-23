66aaa3a05cdec10c21cb3cd768e3bff8bda33d67
Vadim Chetkov <vadim.chetkov@gmail.com>
2016-07-24 00:17:37 +0500

    Changed URLs

--- alchemysite/Game/urls.py
+++ alchemysite/Game/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         'django.contrib.auth.views.login',
         {"template_name": "Game/loginform.html"},
         name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^registration/$', views.registration, name='registration'),
     url(r'get-elements-by-category/(?P<category_id>[0-9]+)',
         views.element_list, name='get-elements-by-cat'),
