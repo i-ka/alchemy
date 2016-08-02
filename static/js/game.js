62f2a84de2517c4191de944f8dfc33a5f09b5a58
Vadim Chetkov <vadim.chetkov@gmail.com>
2016-08-02 20:51:13 +0500

    static/js: elements check

--- static/js/game.js
+++ static/js/game.js
@@ -1,11 +1,51 @@
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      if (cookie.substring(0, name.length + 1) === (name + "=")) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
 $(document).ready(function() {
   $("#board").droppable({
     accept: ".tab-pane a",
     drop: function() {
-      $(this).append($("<a>").attr("el_id", $(".ui-draggable-dragging").attr("el_id")).addClass("btn btn-default").text($(".ui-draggable-dragging").text()).draggable({
-      containment: "parent",
-      stack: ".btn"
-      }));
+      $(this).append($("<a>").attr("el_id", $(".ui-draggable-dragging").attr("el_id")).text($(".ui-draggable-dragging").text()));
+      $("#board a").addClass("btn btn-default").draggable({
+        containment: "parent",
+        stack: ".btn"
+      }).droppable({
+        greedy: true,
+        drop: function() {
+          $(this).addClass("mixable");
+          $(".ui-draggable-dragging").addClass("mixed");
+
+          $.post(
+            "/check_elements/",
+            {
+              first_element: $(".mixable").attr("el_id"),
+              second_element: $(".mixed").attr("el_id"),
+              csrfmiddlewaretoken: getCookie("csrftoken")
+            },
+            onAjaxSuccess
+          );
+
+          function onAjaxSuccess(data) {
+            var elementData = $.parseJSON(data);
+            if (elementData.success) {
+              $(".mixable").text(elementData.newElement.name).attr("el_id", elementData.newElement.id).removeClass("mixable");
+              $(".mixed").remove();
+            }
+          }
+        }
+      });
     }
   });
 });
