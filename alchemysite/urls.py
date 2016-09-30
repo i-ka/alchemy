1be7e5b3a1a20f10efb3644d071a57fce4cf9794
i-ka <i-ka2010@mail.ru>
2016-09-30 20:19:39 +0600

    Urls: media files url added to urlpatterns This is to display screenshots on pages.

--- alchemysite/urls.py
+++ alchemysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 from AlchemyAdmin import urls as aladmin_urls
+from alchemysite import settings
 from Game import urls as game_urls
 
 urlpatterns = [
@@ -24,3 +26,4 @@ urlpatterns = [
     url(r'^alch-admin/',include(aladmin_urls, namespace = 'aladmin')),
     url(r'^',include(game_urls, namespace = 'game')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
