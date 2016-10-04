bfd16fdb7a4cc5513e23b02ed48308fa1ef5fc22
i-ka <i-ka2010@mail.ru>
2016-10-04 22:08:58 +0600

    AlchemyCommon/utils: added redirect_to_next function

--- AlchemyCommon/utils.py
+++ AlchemyCommon/utils.py
@@ -2,6 +2,7 @@ import hashlib
 import random
 from alchemysite import settings
 from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 
 def getActivationEmailText(user):
     hasher = hashlib.sha256((settings.SECRET_KEY + user.email).encode())
@@ -10,3 +11,11 @@ def getActivationEmailText(user):
                     <p>Для подтверждения регистрации перейдите по <a href="{activationUrl}">ссылке</a> </p>
                     <p>Ссылка действительна в течении 1 дня. Если вы не знаете о чем идет речь, просто проигнорируйте это письмо</p>"""
     return hasher.hexdigest(), emailText.format(domain=settings.DOMAIN, activationUrl=activationUrl)
+
+
+def redirect_to_next(request, alt):
+    redirect_url = request.GET.get('next', False)
+    if redirect_url:
+        return redirect(redirect_url)
+    else:
+        return redirect(alt)
