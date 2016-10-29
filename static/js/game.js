7e1d51e8d37137e808c54c6222e5d1fc137fc34d
VChet <vadim.chetkov@gmail.com>
2016-10-29 21:35:55 +0500

    static/js/game.js: refactor

--- static/js/game.js
+++ static/js/game.js
@@ -1,47 +1,62 @@
-$(document).ready(function() {
-  $("#board").droppable({
-    accept: ".tab-pane a",
-    drop: function() {
-      $(this).append($("<a>")
+function addElement(id, name, mixable) {
+  if (!id) {
+    $("#board").append($("<a>")
       .attr("el_id", $(".ui-draggable-dragging").attr("el_id"))
       .css("left", $(".ui-draggable-dragging").css("left"))
       .css("top", $(".ui-draggable-dragging").css("top"))
       .text($(".ui-draggable-dragging").text()));
+  } else {
+    $("#board").append($("<a>")
+      .attr("el_id", id)
+      .css("left", mixable.css("left"))
+      .css("top", mixable.css("top"))
+      .text(name)
+      .effect("fade"));
+  }
 
-      $("#board a").addClass("btn btn-default").draggable({
-        containment: "parent",
-        stack: ".btn"
-      }).droppable({
-        greedy: true,
-        drop: function() {
-          var mixable = $(this);
-          var mixed = $(".ui-draggable-dragging");
-          $.post("/check_elements/", {
-            first_element: mixable.attr("el_id"),
-            second_element: mixed.attr("el_id"),
-            csrfmiddlewaretoken: getCookie("csrftoken")
-          }, onAjaxSuccess);
+  $("#board a").addClass("btn btn-default").draggable({
+    containment: "parent",
+    stack: ".btn"
+  }).droppable({
+    greedy: true,
+    drop: function() {
+      var mixable = $(this);
+      var mixed = $(".ui-draggable-dragging");
+      $.post("/check_elements/", {
+        first_element: mixable.attr("el_id"),
+        second_element: mixed.attr("el_id"),
+        csrfmiddlewaretoken: getCookie("csrftoken")
+      }, onAjaxSuccess);
 
-          function onAjaxSuccess(data) {
-            var elementData = $.parseJSON(data);
-            if (elementData.success) {
-              $("#lastElems").text(mixed.text() + " + " + mixable.text() + " = " + elementData.newElement.name);
-              mixable.text(elementData.newElement.name).attr("el_id", elementData.newElement.id).effect("highlight").width("auto");
-              mixed.remove();
-              if ($(".tab-content [el_id = " + elementData.newElement.id + "]").length == 0) {
-                dataUpdate();
-              }
-            } else {
-              mixable.effect("highlight", { color: "red" });
-              mixed.effect("highlight", { color: "red" });
-            }
+      function onAjaxSuccess(data) {
+        var elementData = $.parseJSON(data);
+        if (elementData.success) {
+          $("#lastElems").text(mixed.text() + " + " + mixable.text() + " = " + elementData.newElement.name);
+          addElement(elementData.newElement.id, elementData.newElement.name, mixable);
+          mixable.remove();
+          mixed.remove();
+          if (!arrElements[elementData.newElement.id]) {
+            dataUpdate();
           }
+        } else {
+          mixable.effect("highlight", { color: "red" });
+          mixed.effect("highlight", { color: "red" });
         }
-      }).click(function() {
-        $("#lastElems").html("<b>" + arrElements[$(this).attr("el_id")].name + "</b> - " + arrElements[$(this).attr("el_id")].description);
-      });
+      }
+    }
+  }).click(function() {
+    $("#lastElems").html("<b>" + arrElements[$(this).attr("el_id")].name + "</b> - " + arrElements[$(this).attr("el_id")].description);
+  });
+}
+
+$(document).ready(function() {
+  $("#board").droppable({
+    accept: ".tab-pane a",
+    drop: function() {
+      addElement();
     }
   });
+
   $("#clearall").click(function() {
     $("#board a").fadeTo(1000, 0, function() {
       $("#board a").remove();
